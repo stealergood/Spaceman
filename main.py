cb856b6ee78d8b6f6b8be362db9878bb8f8ee8d2
Denny Edy Saputra <ncstealer@gmail.com>
2024-06-30 17:38:10 +0700

    modified: main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ bet_template = cv2.imread('./images/bet.png')
 setupbet_template = cv2.imread('./images/setupbet.png')
 finishsetup_template = cv2.imread('./images/notfinish.png')
 
-def run_bot(input_round, mode):
+def run(input_round):
     round = 0
     running = True
     print("Starting the script...")
@@ -33,7 +33,7 @@ def run_bot(input_round, mode):
         screen_image = captureScreen.capture_screen()
         crash_positions = imageDetection.detect_button(screen_image, crash_template, 0.7)
         if crash_positions:
-            crash(mode, running)
+            crash(running)
 
         confirm_positions = imageDetection.detect_button(screen_image, confirm_button_template, 0.7)
         if confirm_positions:
@@ -46,7 +46,7 @@ def run_bot(input_round, mode):
 
     cv2.destroyAllWindows()
 
-def crash(mode, running):
+def crash(running):
     print("Crash detected. Waiting for multiplier.")
     multipler_positions = None
 
@@ -55,7 +55,7 @@ def crash(mode, running):
         multipler_positions = imageDetection.detect_button(screen_image, multipler_template, 0.7)
         if multipler_positions:
             print("Multiplier detected.")
-            clicker.click_button(multipler_positions, mode)
+            clicker.click_button(multipler_positions, 2)
             print("Clicked Multiplier button.")
             time.sleep(0.5)
 
@@ -91,7 +91,7 @@ def crash(mode, running):
                     print("Bet reset.")
 
         elif crash_positions:
-            crash(mode, running)
+            crash(running)
             return
 
 def setup():
@@ -125,5 +125,4 @@ def setup():
 
 if __name__ == '__main__':
     input_round = int(input("Enter number of rounds: "))
-    mode = int(input("Enter mode (1 for x2, 2 for x1.3): "))
-    run_bot(input_round, mode)
+    run(input_round)
